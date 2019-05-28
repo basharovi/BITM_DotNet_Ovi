399ca65b60e664b898c50f42f9775b4e8bd51f58
Your Name <Jarinfarhad58>
2019-05-28 13:43:20 +0600

    TryCatchForm_added

---
+++ TryCatchForm/TryCatchForm/TryCatchForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TryCatchForm
+{
+    partial class CustomerInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayButton = new System.Windows.Forms.Button();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.displayRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(305, 157);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(93, 31);
+            this.displayButton.TabIndex = 0;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(77, 24);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(97, 20);
+            this.userTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(77, 63);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(97, 20);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(77, 106);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(97, 20);
+            this.ageTextBox.TabIndex = 1;
+            this.ageTextBox.TextChanged += new System.EventHandler(this.ageTextBox_TextChanged);
+            this.ageTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ageTextBox_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Age";
+            // 
+            // displayRichTextBox
+            // 
+            this.displayRichTextBox.Location = new System.Drawing.Point(46, 157);
+            this.displayRichTextBox.Name = "displayRichTextBox";
+            this.displayRichTextBox.Size = new System.Drawing.Size(242, 227);
+            this.displayRichTextBox.TabIndex = 3;
+            this.displayRichTextBox.Text = "";
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Font = new System.Drawing.Font("Chaparral Pro", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLabel.Location = new System.Drawing.Point(192, 24);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(0, 19);
+            this.userLabel.TabIndex = 4;
+            // 
+            // CustomerInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 406);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.displayRichTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.displayButton);
+            this.Name = "CustomerInfoForm";
+            this.Text = "Customer Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox displayRichTextBox;
+        private System.Windows.Forms.Label userLabel;
+    }
+}
+
---
+++ TryCatchForm/TryCatchForm/TryCatchForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TryCatchForm
+{
+    public partial class CustomerInfoForm : Form
+    {
+        List<string> users = new List<string>();
+        List<string> names = new List<string>();
+        List<int> ages = new List<int>();
+
+
+        public CustomerInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string user;
+                string name;
+                int age;
+                userLabel.Text = "";
+
+                user = userTextBox.Text;
+                if (String.IsNullOrEmpty(userTextBox.Text))
+                {
+                    MessageBox.Show("User Field can not be Empty");
+                    return;
+                }
+
+                if (UserExist(user))
+                {
+                    userLabel.Text = "User " + user + " Already Exist!!";
+                    return;
+                }
+
+                if (String.IsNullOrEmpty(ageTextBox.Text))
+                {
+                    MessageBox.Show("Age Field can not be Empty");
+                    return;
+                }
+
+                 user = userTextBox.Text;
+                 name = nameTextBox.Text;
+                 age = Convert.ToInt32(ageTextBox.Text);
+
+                users.Add(user);
+                names.Add(user);
+                ages.Add(age);
+
+                 
+
+                displayRichTextBox.Text = Display();
+                
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private string Display()
+        {
+            string message = "";
+            message = "SL\tUser\tName\tAge\n";
+
+            for (int index =0; index < users.Count; index++)
+            {
+                message = message + (index+1) + "\t" + users[index] + "\t" + names[index] + "\t" + ages[index]+ "\n";
+            }
+
+
+            return message;
+        }
+
+        private bool UserExist(string user)
+        {
+            bool isExist = false;
+
+            foreach(string userCheck in users)
+            {
+                if (userCheck == user)
+                    isExist = true;
+            }
+
+            return isExist;
+        }
+
+        private void ageTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(ageTextBox.Text, "  ^ [0-9]"))
+            {
+                ageTextBox.Text = "";
+            }
+        }
+
+        private void ageTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
